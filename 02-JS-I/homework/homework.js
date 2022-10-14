3b3101577aae962d994effbc6cbbfa5a0d479aaf
unknown <wacamachoa@unac.edu.co>
2022-10-14 12:28:38 -0500

    02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = 'Ole';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 2;
 
 // Crea una variable booleana:
 const nuevoBool = null;
